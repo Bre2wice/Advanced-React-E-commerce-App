69e889a55c376dc11a0532d8d6c1702ba6531be4
Brianna Franklin <soul4musiq11@gmail.com>
2025-09-30 00:01:50 -0400

    Add files via upload

---
+++ advanced-ecom/src/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // important!
+  transformIgnorePatterns: ["/node_modules/"],
+};
