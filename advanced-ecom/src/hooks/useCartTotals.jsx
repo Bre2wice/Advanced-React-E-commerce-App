4816c004a3fb31dac1990ab303edfda5734ea000
Brianna Franklin <soul4musiq11@gmail.com>
2025-09-30 13:32:40 -0400

    Delete advanced-ecom directory

--- advanced-ecom/src/hooks/useCartTotals.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-
-export default function useCartTotals() {
-  const items = useSelector((state) => state.cart.items);
-  const totalItems = items.reduce((sum, i) => sum + i.count, 0);
-  const totalPrice = items.reduce((sum, i) => sum + i.price * i.count, 0);
-
-  return { items, totalItems, totalPrice };
-}
